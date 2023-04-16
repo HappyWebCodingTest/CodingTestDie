6acb2f58acea4429f9faefd59bd72167ea49c7a0
Dev-jwJeong <rgn95652@gmail.com>
2023-04-16 20:52:41 +0900

    재욱 : [수학]BOJ10610(Week2 2023.04.16)

--- week2/BOJ10610.js
+++ week2/BOJ10610.js
@@ -1,3 +1,9 @@
+// 30은 10의 배수이기 때문에 무조건 0이 들어가 있어야 30의 배수가 될 수 있다.
+// 0이 있어도 각 자리수의 합이 3의 배수여야 한다.
+// 2개의 조건을 만족해야한다.
+// 그 이후 내림차순으로 정렬하면 제일 큰 수를 알 수 있다.
+// trim으로 공백을 제거해주자
+
 let input = require("fs").readFileSync("input.txt").toString().trim();
 
 if (!input.split("").includes("0")) {
