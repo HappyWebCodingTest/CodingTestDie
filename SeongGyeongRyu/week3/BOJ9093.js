44ac2c815f70b94208e90dc74b3db9a97b8e1c74
pinktopaz <heather128@ewhain.net>
2023-04-23 23:10:32 +0900

    성경 : [문자열] BOJ9093 (Week3 2023.04.23)

---
+++ SeongGyeongRyu/week3/BOJ9093.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux"
+    ? "/dev/stdin"
+    : "Algorithm_basic_200/BOJ9093.txt";
+
+const input = fs.readFileSync(filePath).toString().split("\n");
+const sentences = input.slice(1);
+
+//문자 뒤집기
+sentences.forEach((sentence) => {
+  const letters = sentence.split(" ");
+  for (let i = 0; i < letters.length; i++) {
+    //배열의 순서를 뒤집고 다시 합치는 방식으로 문자열을 뒤집는다.
+    letters[i] = [...letters[i]].reverse().join("");
+  }
+  console.log(letters.join(" "));
+});
+
+//join() => 배열에 쓰는 거, return 안해주기 때문에 변수에 넣어야함
+//reverse() => 배열의 아이템 순서를 바꿔주는 거, 문자열 순서를 바꾸고 싶으면 [...]으로 펼쳐야함
