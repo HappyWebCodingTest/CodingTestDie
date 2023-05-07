1b20a11e88b90b037c55742e35c75e5358104fbc
Dev-jwJeong <rgn95652@gmail.com>
2023-05-07 23:12:45 +0900

    재욱 : [스택]BOJ1874(Week5 2023.05.06)

---
+++ JaeWook/week5/BOJ1406.js
@@ -0,0 +1,22 @@
+// 커서를 파악하는게 중요한 문제였다.
+// 스택을 사용해서 커서를 중심으로 왼쪽과 오른쪽 스택을 나누어서 생각했다.
+// 스택을 두개사용해서 푸는게 핵심!
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().split("\n");
+
+let lStack = input[0].split("");
+let rStack = [];
+let len = parseInt(input[1]);
+
+for (let i = 2; i < 2 + len; i++) {
+  let [cmd, value] = input[i].split(" ");
+  if (cmd === "L" && lStack.length) rStack.push(lStack.pop());
+  else if (cmd === "D" && rStack.length) lStack.push(rStack.pop());
+  else if (cmd === "B") lStack.pop();
+  else if (cmd === "P") lStack.push(value);
+}
+
+let answer = lStack.join("");
+answer += rStack.reverse().join("");
+console.log(answer);
